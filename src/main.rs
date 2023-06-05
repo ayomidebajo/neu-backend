78feca273320a7869a2bdc3f505b052c09f02313
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-06-05 10:56:44 +0100

    add feature fill db with mock data

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
+use std::{fs::File, io::{self, BufRead}};
+use std::io::BufReader;
+use serde_json::{Result as SerdeResult, Value};
+
 // Title: Rust Postgres
    use postgres::{Client, NoTls, Error};
    use clap::{Parser, Command, ArgAction, Arg};
 
-// #[derive(Parser, Debug)]
-// #[command(author, about, version, long_about, name = "Neu Backend")]
-// struct Args {
-//     add: String,
-//     create: String, 
-// }
+
+//  TODO: Change API
 
 fn main() -> Result<(), Error>{
     
@@ -30,13 +30,9 @@ fn main() -> Result<(), Error>{
            .subcommand(Command::new(CMD_ADD).about("add user to the table")
                        .arg(Arg::new("name")
                             .help("Sets the name of a user")
+                            .value_name("FILE")
                             .index(1)
-                            .required(true))
-                       .arg(Arg::new("email")
-                            .help("Sets the email of a user")
-                            .index(2)
-                            .required(true)
-                         ))
+                            .required(true)))
            .subcommand(Command::new(CMD_LIST).about("print list of users"))
            .get_matches();
 
@@ -53,8 +49,33 @@ fn main() -> Result<(), Error>{
             },
             Some((CMD_ADD, matched)) => {
            let name = matched.get_one::<String>("name").unwrap();
-           let email = matched.get_one::<String>("email").unwrap();
-           create_user(&mut conn, name, email)?;
+
+           let file = File::open(name).expect("error opening file");
+
+         let reader = BufReader::new(file);
+
+    // Deserialize the JSON data
+    let data: Value = serde_json::from_reader(reader).expect("error while reading json");
+
+           // Iterate through the JSON object
+    if let Some(items) = data.as_array() {
+        let mut user_collections: Vec<(&str, &str)> = Vec::new();
+        for item in items {
+            // Access individual fields or values within each item
+            let name = item.get("name").expect("error getting name");
+            let email = item.get("email").expect("error getting email");
+
+            //  push to vector
+            user_collections.push((name.as_str().unwrap(), email.as_str().unwrap()));
+
+            println!("user_collections {:?}", user_collections);
+        }
+
+        for (name, email) in user_collections {
+            create_user(&mut conn, name, email)?;
+        }
+    }
+        println!("name {}", name);
             },
             Some((CMD_LIST, _)) => {
                 println!("list");
@@ -70,13 +91,15 @@ fn main() -> Result<(), Error>{
         }
 
         Ok(())
-    // println!("out: {}", out);
+
+
 }
 
 fn create_table(conn: &mut Client) -> Result<(), Error> {
        conn.execute("CREATE TABLE users ( id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, email VARCHAR UNIQUE )", &[])
            .map(drop)
 }
+
 // This function demonstrates how to insert data into the database.
 // TODO: return the id of the inserted row or the email address so that we can send the user a confirmation email.
 fn create_user(conn: &mut Client, name: &str, email: &str) -> Result<(), Error> {
@@ -84,6 +107,8 @@ fn create_user(conn: &mut Client, name: &str, email: &str) -> Result<(), Error> 
                     &[&name, &email])
            .map(drop)
 }
+
+
 
 
 fn list_users(conn: &mut Client) -> Result<Vec<(String, String)>, Error> {
