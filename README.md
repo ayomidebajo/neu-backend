486e5e9ada90c5f542c1380ba994d2468e46a68b
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-06-09 15:14:52 +0100

    add import feature

--- README.md
+++ README.md
@@ -6,5 +6,12 @@
 ## add a user to db
 `cargo run -- --db postgres://postgres@localhost:5432 add user-1 user-1@example.com`
 
+## create users table
+`cargo run -- --db postgres://postgres@localhost:5432 create`
+
+## import users from json file
+`cargo run -- --db postgres://postgres@localhost:5432 import temp-db.json`
+
 ## list users
 `cargo run -- --db postgres://postgres@localhost:5432 list`
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use r2d2_postgres::PostgresConnectionManager;
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use serde_derive::Deserialize;
 use serde_json::{Result as SerdeResult, Value};
 use std::io::BufReader;
@@ -7,7 +9,7 @@ use std::{
 };
 
 // Title: Rust Postgres
-use clap::{Arg, ArgAction, Command, Parser};
+use clap::{arg, Arg, ArgAction, Command, Parser};
 use postgres::{Client, Error, NoTls};
 
 //  TODO: Change API
@@ -29,7 +31,7 @@ impl User {
     }
 }
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), failure::Error> {
     const CMD_CREATE: &str = "create";
     const CMD_ADD: &str = "add";
     const CMD_LIST: &str = "list";
@@ -47,21 +49,35 @@ fn main() -> Result<(), Error> {
         )
         .subcommand(Command::new(CMD_CREATE).about("create users table"))
         .subcommand(
-            Command::new(CMD_ADD).about("add user to the table").arg(
-                Arg::new("name")
-                    .help("Sets the name of a user")
-                    .value_name("FILE")
-                    .index(1)
-                    .required(true),
-            ),
+            Command::new(CMD_ADD)
+                .about("add user to the table")
+                .arg(
+                    Arg::new("name")
+                        .help("Sets the name of a user")
+                        .index(1)
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("email")
+                        .help("Set the email of a user")
+                        .index(2)
+                        .required(true),
+                ),
         )
         .subcommand(Command::new(CMD_LIST).about("print list of users"))
-        .subcommand(Command::new(CMD_IMPORT).about("import users from json file"))
+        .subcommand(
+            Command::new(CMD_IMPORT)
+                .about("import users from json file")
+                .arg(Arg::new("name").help("add file").value_name("FILE").required(true)),
+        )
         .get_matches();
 
     let addr = matches.clone();
     let addr = addr.get_one::<String>("database").unwrap();
-    let mut conn = Client::connect(&addr, NoTls).unwrap();
+    let manager = PostgresConnectionManager::new(addr.parse().unwrap(), NoTls);
+    // let mut conn = Client::connect(&addr, NoTls).unwrap();
+    let pool = r2d2::Pool::new(manager)?;
+    let mut conn = pool.get()?;
 
     match matches.subcommand() {
         Some((CMD_CREATE, _)) => {
@@ -69,8 +85,8 @@ fn main() -> Result<(), Error> {
             // create_table(&mut conn).unwrap();
         }
         Some((CMD_ADD, matched)) => {
-            let name = matched.get_one::<String>("NAME").unwrap().to_owned();
-            let email = matched.get_one::<String>("EMAIL").unwrap().to_owned();
+            let name = matched.get_one::<String>("name").unwrap().to_owned();
+            let email = matched.get_one::<String>("email").unwrap().to_owned();
             let user = User { name, email };
             create_user(&mut conn, &user)?;
         }
@@ -78,7 +94,7 @@ fn main() -> Result<(), Error> {
             println!("list");
             let users = list_users(&mut conn)?;
             for user in users {
-                 println!("Name: {:20}    Email: {:20}", user.name, user.email);
+                println!("Name: {:20}    Email: {:20}", user.name, user.email);
             }
         }
         Some((CMD_IMPORT, matched)) => {
@@ -94,6 +110,21 @@ fn main() -> Result<(), Error> {
             // Iterate through the JSON object
             if let Some(items) = data.as_array() {
                 let mut user_collections: Vec<(&str, &str)> = Vec::new();
+                user_collections
+                    .par_iter()
+                    .map(|item| {
+                        let mut conn = pool.get().expect("error getting connection");
+                        let name = item.0;
+                        let email = item.1;
+
+                        let user = User {
+                            name: name.to_string(),
+                            email: email.to_string(),
+                        };
+
+                        create_user(&mut conn, &user)
+                    })
+                    .for_each(drop);
                 for item in items {
                     // Access individual fields or values within each item
                     let name = item.get("name").expect("error getting name");
