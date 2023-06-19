39c99c9322f10435ab7238a9ea551432b2c736c9
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-06-19 19:41:16 +0100

    clean up code

--- src/helpers/pass_helpers.rs
+++ src/helpers/pass_helpers.rs
@@ -9,8 +9,5 @@ pub fn hash_password(password: &str) -> Result<String, bcrypt::BcryptError> {
 
 pub fn verify_password(password: &str, hashed_password: &str) -> bool {
     // Verify the provided password against the stored hashed password
-    match verify(password, hashed_password) {
-        Ok(result) => result,
-        Err(_) => false, // Verification failed
-    }
+    verify(password, hashed_password).unwrap_or(false)
 }
