399d2b7ff93edbf12fe51db35be2c14d30fe5efc
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-06-23 15:05:59 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,8 @@ use neu_backend::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = TcpListener::bind("127.0.0.1:0")?;
