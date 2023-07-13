fd7a762a88556a10d91066e8e51bb890069de565
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-07-13 12:17:38 +0100

    remove dead code

--- src/lib.rs
+++ src/lib.rs
@@ -18,14 +18,6 @@ pub fn run(
         App::new()
             .wrap(TracingLogger::default())
             .configure(routes::handler::config)
-            // .route(
-            //     "/health_check",
-            //     web::get().to(routes::health_check::health_check),
-            // )
-            // .route("/home", web::get().to(routes::home_page::home_page))
-            // .route("/sign_up", web::post().to(routes::sign_up::sign_up))
-            // .route("/login", web::post().to(routes::login::sign_in))
-            // .route("/me", web::get().to(routes::login::get_me_handler))
             .app_data(web::Data::new(config::AppState {
                 db: connection.clone(),
                 config: config.clone(),
