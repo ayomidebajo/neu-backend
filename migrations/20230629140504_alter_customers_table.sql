89972f418dc7951dbe7bbf250698520d76b2ec21
Ayomide Bajo <oluwashinabajo@gmail.com>
2023-07-14 14:49:00 +0100

    add sql migration

--- migrations/20230629140504_alter_customers_table.sql
+++ migrations/20230629140504_alter_customers_table.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
--- ALTER TABLE customers
--- ADD COLUMN IF NOT EXISTS is_subscribed BOOLEAN DEFAULT false;
+ALTER TABLE customers
+ADD COLUMN IF NOT EXISTS is_subscribed BOOLEAN DEFAULT false;
 
 ALTER TABLE customers
 DROP IF EXISTS is_merchant;
